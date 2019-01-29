6dbfe0131132606aed0e8b08bbe942d37630e353
eric10499 <34514368+eric10499@users.noreply.github.com>
2019-01-29 20:00:32 +0100

    Create hibernateVE.java

---
+++ hibernateVE.java
@@ -0,0 +1,25 @@
+import java.util.Iterator;
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import primer.HibernateUtil;
+
+public class hibernateVE {
+	public static void main(String[] args){
+		SessionFactory sesion = HibernateUtil.getSessionFactory();
+		Session session = sesion.openSession();
+		Query q = session.createQuery("select avg(e.salario), count(apellido) from Emple e group by e.depart");
+		List <?> lista =q.list();
+		Iterator <?> iter = lista.iterator();
+		Object[] listaObj;
+		while (iter.hasNext()){
+			listaObj = (Object[]) iter.next();
+			System.out.println(listaObj[0] + " - " + listaObj[1]);
+		}
+		session.close();
+		System.exit(0);
+	}
+}
