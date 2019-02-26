f75823ecead3afc53d64b9f990dd9627f18ccd83
eric10499 <34514368+eric10499@users.noreply.github.com>
2019-02-26 18:23:33 +0100

    Create Jugadores.java

---
+++ Jugadores.java
@@ -0,0 +1,54 @@
+package m6BBOO;
+
+public class Jugadores {
+	private Paises pais;
+	private String nombre;
+	private String deporte;
+	private String ciudad;
+	private int edad;
+	
+	
+	public Jugadores(Paises pais, String nombre, String deporte, String ciudad, int edad) {
+		super();
+		this.pais = pais;
+		this.nombre = nombre;
+		this.deporte = deporte;
+		this.ciudad = ciudad;
+		this.edad = edad;
+	}
+	
+	public Paises getPais() {
+		return pais;
+	}
+	public void setPais(Paises pais) {
+		this.pais = pais;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getDeporte() {
+		return deporte;
+	}
+	public void setDeporte(String deporte) {
+		this.deporte = deporte;
+	}
+	public String getCiudad() {
+		return ciudad;
+	}
+	public void setCiudad(String ciudad) {
+		this.ciudad = ciudad;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	
+	
+	
+}
