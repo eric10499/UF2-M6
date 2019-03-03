2eb5ece19c2b59aa66f669289c0a9702d8ca0634
eric10499 <34514368+eric10499@users.noreply.github.com>
2019-03-03 11:37:48 +0100

    Proyecto NeoDatis

---
+++ Depart.java
@@ -0,0 +1,59 @@
+
+public class Depart {
+
+	private int deptNo; 
+	private String dnombre; 
+	private String loc;
+
+
+
+	public Depart(int deptNo, String dnombre, String loc) {
+		super();
+		this.deptNo = deptNo;
+		this.dnombre = dnombre;
+		this.loc = loc;
+	}
+
+
+	public int getDeptNo() {
+		return deptNo;
+	}
+
+
+
+	public void setDeptNo(int deptNo) {
+		this.deptNo = deptNo;
+	}
+
+
+
+	public String getDnombre() {
+		return dnombre;
+	}
+
+
+
+	public void setDnombre(String dnombre) {
+		this.dnombre = dnombre;
+	}
+
+
+
+	public String getLoc() {
+		return loc;
+	}
+
+
+
+	public void setLoc(String loc) {
+		this.loc = loc;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "Depart [deptNo=" + deptNo + ", dnombre=" + dnombre + ", loc=" + loc + "]";
+	} 
+
+}
